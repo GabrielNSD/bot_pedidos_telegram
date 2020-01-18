eedde5d7ff279daa6e3c01291ab71d8968b54d3e
Gabriel Dantas <gabrielnsd@protonmail.com>
2020-01-18 19:09:04 +0300

    Bot is answering to the user and also copying the message to group

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import requests
 import json
 import configparser as cfg
 
-class telegram_chatbot():
+
+class TelegramChatBot():
     def __init__(self, config):
         self.token = self.read_token_from_config_file(config)
         self.base = "http://api.telegram.org/bot{}/".format(self.token)
 
     def get_updates(self, offset=None):
-        url = self.base + "/getUpdates?timeout=100"
+        url = self.base + "getUpdates?timeout=10"
         if offset:
             url = url + "&offset={}".format(offset + 1)
         r = requests.get(url)
@@ -18,7 +19,12 @@ class telegram_chatbot():
         url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
         if msg is not None:
             requests.get(url)
-    
+
+    def send_group_message(self, msg, chat_id):
+        url = self.base + "sendMessage?chat_id=-{}&text={}".format(chat_id, msg)
+        if msg is not None:
+            requests.get(url)
+
     def read_token_from_config_file(self, config):
         parser = cfg.ConfigParser()
         parser.read(config)
